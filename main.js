24b03449899e3ab2355efaa6c709484de5b59a3b
mali-ai <76814802+mali-ai@users.noreply.github.com>
2021-11-01 21:09:16 +0500

    Implemented Complete Functionality

---
+++ main.js
@@ -0,0 +1,48 @@
+async function getUsers() {
+    let url = 'https://jsonplaceholder.typicode.com/users';
+    try {
+        let result = await fetch(url);
+        return await result.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getUsersPosts() {
+    let url = 'https://jsonplaceholder.typicode.com/posts';
+    try {
+        let result = await fetch(url);
+        return await result.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getUserPostsById(id){
+    let posts = await getUsersPosts();
+    let html = `<div style="margin-left:auto; margin-right:auto; width: 50%;" id="postByUser${id}">`;
+    posts.forEach(post => {
+        if (post.userId == id) {
+            html = html + `<pre>Post ID: ${post.id}     User ID: ${post.userId}</pre><p>Post Title: ${post.title}</p><p>Post Body: ${post.body}</p><hr>`;
+        }
+    });
+    html = html + `</div>`;
+    let container = document.getElementById('user' + id);
+    container.innerHTML = html;
+}
+
+async function displayUsers() {
+    let users = await getUsers();
+    let html = '';
+    users.forEach(user => {
+        html = html + `<button style="margin-left:auto; margin-right:auto; display: block; width: 50%;" onclick="togglePosts(${user.id})">${user.name}</button><div style="display: none;" id="user${user.id}"></div>`;
+    });
+
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+function togglePosts(id) {
+    getUserPostsById(id);
+    $("#user" + id).toggle();
+}
