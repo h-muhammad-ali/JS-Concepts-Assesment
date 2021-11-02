5a5c9e9fd502745469f5b565e8a5240496eecf7e
mali-ai <76814802+mali-ai@users.noreply.github.com>
2021-11-02 16:58:19 +0500

    [JCS-1] Fetching Data from API (#1)
    * Implemented Complete Functionality
    
    * Styling Added and Finalized
    
    * Arrow Function Syntax Used
    
    * let Changed to const in Arrow Function Expressions

---
+++ main.js
@@ -0,0 +1,52 @@
+const getUsers = async () => {
+  let url = "https://jsonplaceholder.typicode.com/users";
+  try {
+    let result = await fetch(url);
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getUsersPosts = async () => {
+  let url = "https://jsonplaceholder.typicode.com/posts";
+  try {
+    let result = await fetch(url);
+    return await result.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const getUserPostsById = async (id) => {
+  let posts = await getUsersPosts();
+  let html = `<div style="margin-left:auto; margin-right:auto; width: 50%;" id="postByUser${id}" class="posts">`;
+  posts.forEach((post) => {
+    if (post.userId == id) {
+      html =
+        html +
+        `<pre>Post ID: ${post.id}     User ID: ${post.userId}</pre><p>Post Title: ${post.title}</p><p>Post Body: ${post.body}</p><hr>`;
+    }
+  });
+  html = html + `</div>`;
+  let container = document.getElementById("user" + id);
+  container.innerHTML = html;
+}
+
+const displayUsers = async () => {
+  let users = await getUsers();
+  let html = "";
+  users.forEach((user) => {
+    html =
+      html +
+      `<button style="margin-left:auto; margin-right:auto; display: block; width: 50%;" class="users" onclick="togglePosts(${user.id})">${user.name}</button><div style="display: none;" id="user${user.id}"></div>`;
+  });
+
+  let container = document.querySelector(".container");
+  container.innerHTML = html;
+}
+
+const togglePosts = async (id) => {
+  getUserPostsById(id);
+  $("#user" + id).toggle();
+}
